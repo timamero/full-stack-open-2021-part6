3f34add88b989962d9f46eab4a239e2ee17af4f6
Fatima Camero <fcamero12@gmail.com>
2021-10-07 23:05:33 -0700

    Remove comments

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,48 +1,12 @@
 import React from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { voteOnAnecdote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  // const anecdotes = useSelector(state => state)
-  // const dispatch = useDispatch()
-  
-  // const vote = (id) => {
-  //   console.log('vote', id)
-  //   dispatch(voteOnAnecdote(id))
-  // }
-
-  // const sortByVote = (a, b) => {
-  //   if (a.votes > b.votes) {
-  //     return -1
-  //   }
-
-  //   if (a.votes < b.votes) {
-  //     return 1
-  //   }
-
-  //   return 0;
-  // }
-
-  // const sortedAnecdotes = anecdotes.sort(sortByVote)
-  // console.log('sortedAnecdotes', sortedAnecdotes)
-
   return (
     <div>
       <h2>Anecdotes</h2>
       <AnecdoteList />
-      {/* {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )} */}
       <AnecdoteForm />
     </div>
   )
