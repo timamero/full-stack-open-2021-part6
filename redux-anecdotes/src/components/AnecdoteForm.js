f0a7aded37159d92cc04df9c1564fe16db97a5ee
Fatima Camero <fcamero12@gmail.com>
2021-10-07 22:51:59 -0700

    Move creation of new anecdotes into AnecdoteForm component

---
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { createAnecdote } from '../reducers/anecdoteReducer';
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+  
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const newAnecdote = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    console.log('newAnecdote', newAnecdote)
+    dispatch(createAnecdote(newAnecdote))
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote"/></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
