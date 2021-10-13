7d8b3f6040c9d81eae2639e30edb0b87d918230d
Fatima Camero <fcamero12@gmail.com>
2021-10-13 11:22:10 -0700

    move store to separate file

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+// import { createStore } from 'redux'
+// import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import store from './store'
+// import reducer from './reducers/anecdoteReducer'
 
-const store = createStore(
-  reducer,
-  composeWithDevTools()
-)
+// const store = createStore(
+//   reducer,
+//   composeWithDevTools()
+// )
 
 ReactDOM.render(
   <Provider store={store}>
